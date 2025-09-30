8ef407eefdd7673f0c33983ed4874823b9553569
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2025-09-30 20:12:22 -0300

    adiciona seeder de usuário

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'type' => 'admin',
+                'name' => 'Admin',
+                'username' => "admin",
+                'password' => Hash::make("admin"),
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ]);
+    }
+}
