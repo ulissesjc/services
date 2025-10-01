f124c82e41cc3a2186cd6fcf38fc32e388502518
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2025-10-01 20:39:05 -0300

    altera cascata das escolas da barra lateral

--- resources/views/components/side-bar/create-list-school.blade.php
+++ resources/views/components/side-bar/create-list-school.blade.php
@@ -1,10 +1,12 @@
 <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small ps-4">
-    <li>
-        <a href="{{ route('school-create') }}" class="nav-link text-white">
-            <i class="fa-solid fa-plus"></i>
-            Cadastrar
-        </a>
-    </li>
+    @if(auth()->user()->isAdmin())
+        <li>
+            <a href="{{ route('school-create') }}" class="nav-link text-white">
+                <i class="fa-solid fa-plus"></i>
+                Cadastrar
+            </a>
+        </li>
+    @endif
     <li>
         <a href="{{ route('school-index') }}" class="nav-link text-white">
             <i class="fa-solid fa-list"></i>
