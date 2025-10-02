49e4b4a3dbe5069b1eb7a4924bad58c89c38205b
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2025-10-01 21:25:09 -0300

    melhora tela de acesso negado

--- resources/views/403-error.blade.php
+++ resources/views/403-error.blade.php
@@ -3,9 +3,13 @@
 @section('title', 'Acesso Negado')
 
 @section('content')
-    <div class="text-center mt-5">
-        <h1 class="text-danger">Acesso negado</h1>
-        <p>Você não tem permissão para acessar essa página!</p>
-        <a href="javascript:history.back()" class="btn btn-primary">Voltar</a>
+    <div class="d-flex flex-column vh-100">
+        <div class="text-center mt-auto mb-auto">
+            <h1 class="text-danger mb-2">ACESSO NEGADO!</h1>
+            <p>Você não tem permissão para acessar essa página!</p>
+        </div>
+        <div class="ms-3 mb-5">
+            <a href="javascript:history.back()" class="btn btn-primary">VOLTAR</a>
+        </div>
     </div>
 @endsection
